1b6357c5d486ae84290fe9f548a7801c81c4a4b0
Zachary Davis <test106437@gmail.com>
2025-05-11 00:31:03 +0300

    Board works

--- board.cbl
+++ board.cbl
@@ -11,6 +11,9 @@
 
        WORKING-STORAGE SECTION.
 
+       77  ROW-IDX      PIC 9 VALUE 1.
+       77  COL-IDX      PIC 9 VALUE 1.
+
        LINKAGE SECTION.
        01  TIC-TAC-TOE-BOARD.
            05 ROW OCCURS 3 TIMES.
@@ -18,7 +21,23 @@
                    15 CELL-VALUE PIC X VALUE SPACE.
 
        PROCEDURE DIVISION USING TIC-TAC-TOE-BOARD.
-           DISPLAY "SUCCESSFULLY CALLED".
+
+           DISPLAY CELL-VALUE (2, 3)
+           PERFORM VARYING ROW-IDX FROM 1 BY 1 UNTIL ROW-IDX > 3
+               PERFORM VARYING COL-IDX FROM 1 BY 1 UNTIL COL-IDX > 3
+
+                   DISPLAY CELL-VALUE (ROW-IDX, COL-IDX) WITH NO
+                   ADVANCING
+
+                   IF COL-IDX < 3
+                       DISPLAY " | " WITH NO ADVANCING
+                   END-IF
+               END-PERFORM
+               DISPLAY ""   *> move to next line after each row
+               IF ROW-IDX < 3
+                   DISPLAY "--+---+--"
+               END-IF
+           END-PERFORM
 
       *----VOID FUNCTION NO RETURN
            EXIT PROGRAM.
--- tictactoe.cbl
+++ tictactoe.cbl
@@ -18,6 +18,10 @@
        FILE SECTION.
       *-----------------------
        WORKING-STORAGE SECTION.
+       01  PLAYER-MOVE PIC X(4).
+
+       77  FIRST-NUMBER      PIC 99.
+       77  SECOND-NUMBER     PIC 99.
 
        01  TIC-TAC-TOE-BOARD.
            05 ROW OCCURS 3 TIMES.
@@ -31,7 +35,23 @@
       **
       * The main procedure of the program
       **
-            DISPLAY "Hello world"
+
+           PERFORM FOREVER
+               DISPLAY "YOUR MOVE (X,X): "
+               ACCEPT PLAYER-MOVE
+
+               MOVE PLAYER-MOVE(1:1) TO FIRST-NUMBER
+               MOVE PLAYER-MOVE(2:2) TO SECOND-NUMBER
+
+               DISPLAY FIRST-NUMBER
+               DISPLAY SECOND-NUMBER
+
+               MOVE "X" TO CELL-VALUE (FIRST-NUMBER, SECOND-NUMBER)
+
+               CALL "BOARD" USING TIC-TAC-TOE-BOARD
+
+           END-PERFORM
+
             STOP RUN.
       ** add other procedures here
        END PROGRAM TIC-TAC-TOE.
